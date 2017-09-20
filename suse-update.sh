d52dab40ba91bb0d4a1a0314a7b4cb19ffebd730
Dan Huckson <danhuckson@hotmail.com>
2017-09-20 16:10:10 -0400

    Add files via upload
    Uploaded first version 1.00 of suse-update.sh. suse-update.sh updates openSUSE tumbleweed with the latest packages. Use this cript with CRON to automate tumbleweed updates.

---
+++ suse-update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+###############################################################################
+#  Script: suse-update.sh
+# Purpose: Update openSUSE tumbleweed with the latest packages.
+# Version: 1.00
+#  Author: Dan Huckson
+#    Date: 2017/09/20
+###############################################################################
+
+start_time=$(date +%s)
+date >> /tmp/suse-update-timestamps.txt
+
+if [ -z "$1" ]; then VERBOSITY=0; else VERBOSITY=$1; fi
+
+if (( $VERBOSITY > 1 )) || (( $VERBOSITY < 0 )); then
+    echo "Incorrect value entered."
+    exit
+fi
+
+if (( $VERBOSITY )); then
+    zypper refresh > /dev/nil
+    echo Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories
+    zypper -v -n update --auto-agree-with-licenses | grep -P "^Nothing to do|^CommitResult  \(|The following \d{1}" | sed 's/The following //' | tee /var/log/suse-update.log
+else
+    echo -e "\nRefreshing Repositories" | tee /var/log/suse-update.log
+    echo -e "----------------------------------------" | tee -a /var/log/suse-update.log
+    zypper refresh | cut -d"'" -f2 | tee -a /var/log/suse-update.log
+    echo -e "----------------------------------------\n" | tee -a /var/log/suse-update.log
+    zypper -v -n update --auto-agree-with-licenses | sed "/Unknown media type in type/d;s/^   //;/^Additional rpm output:/d" | sed ':a;N;$!ba;s/\n  / /g' | tee -a /var/log/suse-update.log
+fi
+
+s=$[$(date +%s) - $start_time]; h=$[$s / 3600]; s=$[$s - $[$h * 3600]]; m=$[$s / 60]; s=$[$s - $[m * 60]]
+[ "$h" != '0' ] && hours=" $h hours" || hours=""
+[ "$m" != '0' ] && minutes=" $m minutes and" || minutes=""
+echo -e "\nTotal update time$hours$minutes $s seconds." | tee -a /var/log/suse-update.log

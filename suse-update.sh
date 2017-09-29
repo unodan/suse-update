bd49cd486557f712c4992ac8e5bbaf788713d4e7
Dan Huckson <danhuckson@hotmail.com>
2017-09-29 15:43:52 -0400

    Added error checking
    Added error checking and tidied up somethings.

--- suse-update.sh
+++ suse-update.sh
@@ -2,12 +2,11 @@
 ###############################################################################
 #  Script: suse-update.sh
 # Purpose: Update openSUSE tumbleweed with the latest packages.
-# Version: 1.29
+# Version: 1.30
 #  Author: Dan Huckson
 ###############################################################################
 date=`date`
 time=$(date +%s)
-
 script=$(basename -- "$0")
 distribution="openSUSE tumbleweed"
 
@@ -17,11 +16,13 @@ log="$dir/$name".log
 
 if [ ! -d "$dir" ]; then mkdir $dir; fi
 
-while getopts ":rvhk:l" opt; do
+while getopts ":rvlhk:" opt; do
   case $opt in
     r)  reboot=1 
         ;;
     v)  verbosity=1 
+        ;;
+    l)  auto_agree_with_licenses="--auto-agree-with-licenses"
         ;;
     h)  echo -e "\nUsage: $script [OPTION]..."
         echo -e "Update $distribution with the latest packages"
@@ -43,9 +44,7 @@ while getopts ":rvhk:l" opt; do
             echo -e "\nUse $script -h for more information." >&2
             exit 20
         fi
-        cd $dir && ls -tp | grep -v '/$' | tail -n +$OPTARG | xargs -d '\n' -r rm -- 
-        ;;
-    l)  auto_agree_with_licenses="--auto-agree-with-licenses"
+        cd $dir && ls -tp | grep -v '/$' | tail -n +$OPTARG | xargs -rd '\n' rm -- 
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -60,28 +59,35 @@ while getopts ":rvhk:l" opt; do
   esac
 done
 
-echo $date >> /tmp/$name-datestamp.txt
+echo -e "Start: $date" | tee -a $log
+(( $auto_agree_with_licenses )) && { 
+    echo -e "\nApplying updates without asking to confirm licenses.\n" >&2; 
+}
 
 if (( $verbosity )); then
     echo -e "\nRefreshing Repositories" | tee -a $log
     echo -e "----------------------------------------" | tee -a $log
     zypper refresh | cut -d"'" -f2 | tee -a $log
+    (( $? )) && { echo "An error occurred with (zypper refresh) exiting script." >&2; exit 50; } 
     echo -e "----------------------------------------\n" | tee -a $log
     
-    if [ ! -z $auto_agree_with_licenses ]; then
-        echo -e "Applying updates to the system without asking to confirm any licenses.\n" >&2 
-    fi
     
     zypper -v -n update $auto_agree_with_licenses | sed "/Unknown media type in type/d;s/^   //;/^Additional rpm output:/d" | sed ':a;N;$!ba;s/\n  / /g' | tee -a $log
+    (( $? )) && { echo "An error occurred with (zypper update) exiting script."  >&2; exit 55; } 
 else
     zypper refresh > /dev/nil
+    (( $? )) && { echo "An error occurred with (zypper refresh) exiting script."  >&2; exit 60; } 
     echo Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories
+    
     zypper -v -n update $auto_agree_with_licenses | grep -P "^Nothing to do|^CommitResult  \(|The following \d{1}" | sed 's/The following //' | tee -a $log
+    (( $? )) && { echo "An error occurred with (zypper update) exiting script."  >&2; exit 65; } 
 fi
 
 s=$[$(date +%s) - $time]; h=$[$s / 3600]; s=$[$s - $[$h * 3600]]; m=$[$s / 60]; s=$[$s - $[m * 60]]
 [ "$h" != '0' ] && hours=" $h hours" || hours=""
 [ "$m" != '0' ] && minutes=" $m minutes and" || minutes=""
+
+echo -e "\nEnd: `date`\n" | tee -a $log
 echo -e "Total run time$hours$minutes $s seconds." | tee -a $log
 
 if (( $reboot )); then init 6; fi

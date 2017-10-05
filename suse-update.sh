5575a9ddac4752c9993fb5ced2d6e8c151152a43
Dan Huckson <danhuckson@hotmail.com>
2017-10-04 21:03:53 -0400

    Added two new options and code optimizations
    Added option to refresh all the enabled repositories.
    Added the option to archive log files when they equal the value supplied to the -k option.
    Many changes where done to the code logic for optimization and better readability.

--- suse-update.sh
+++ suse-update.sh
@@ -2,7 +2,7 @@
 ###############################################################################
 #  Script: suse-update.sh
 # Purpose: Update openSUSE tumbleweed with the latest packages.
-# Version: 1.31
+# Version: 2.0
 #  Author: Dan Huckson
 ###############################################################################
 date=`date`
@@ -12,46 +12,59 @@ distribution="openSUSE tumbleweed"
 
 name=`echo $script | cut -f1 -d.`
 dir=/var/log/$name
-log="$dir/$name".log
+log=$dir/$name.log
 
-if [ ! -d "$dir" ]; then mkdir $dir; fi
+[ ! -d "$dir" ] && mkdir $dir; 
 
-while getopts ":rvlhk:" opt; do
+while getopts ":rlvak:sh" opt; do
   case $opt in
-    r)  reboot=1 
-        ;;
-    v)  verbosity=1 
+    r)  refresh=1
         ;;
     l)  auto_agree_with_licenses="--auto-agree-with-licenses"
         ;;
-    h)  echo -e "\nUsage: $script [OPTION]..."
-        echo -e "Update $distribution with the latest packages"
-        echo -e "\n\t-r\t Reboot after update"
-        echo -e "\t-v\t Verbosity (show maximum information)"
-        echo -e "\t-l\t Auto agree with licenses"
-        echo -e "\t-h\t Display this help message"
-        echo -e "\t-k\t Maximum number of log files to keep,"
-        echo -e "\t\t this option must be supplied with a numeric value"
-        echo -e "\nExample: $script -vrk 30 "
-        echo -e "  output maximum info, reboot and keep the latest 30 log files."
-        exit 10
+    v)  verbosity=1 
         ;;
-    k)  log="$dir/$name-`date +%Y%m%d-%H%M%S`.log"
-        if ! [[ $OPTARG =~ ^[0-9]+$ ]]; then
-            echo -e "\nPlease enter a positive interger value for the maximum number of log files to keep.\n" >&2
+    a)  number_of_log_files=`ls $dir/$name*.log 2> /dev/null | wc -l`
+    
+        (( $maximum_log_files )) && (( $number_of_log_files >= $maximum_log_files )) && {
+            [ ! -d $dir/archive ] && mkdir -p $dir/archive; 
+            
+            logs="$name-logs-`date +%Y%m%d-%H%M%S`" 
+            ls $dir/*.log | xargs zip -q $dir/archive/$logs.zip
+            rm $dir/*.log
+        }
+        ;;
+    k)  maximum_log_files=$OPTARG    
+    
+        ! [[ $maximum_log_files =~ ^[0-9]+$ ]] && {
+            echo -e "\nERROR: Please enter a positive interger value for the maximum number of log files to keep.\n" >&2
             echo -e "Example: $script -vrk 30 " >&2
             echo -e "  output maximum info, reboot and keep the latest 30 log files." >&2
             echo -e "\nUse $script -h for more information." >&2
-            exit 20
-        fi
-        cd $dir && ls -tp | grep -v '/$' | tail -n +$OPTARG | xargs -rd '\n' rm -- 
+            exit 10
+        }
+        log="$dir/$name-`date +%Y%m%d-%H%M%S`.log"
         ;;
-    \?)
-        echo -e "Invalid option: -$OPTARG\nUse $script -h for more information." >&2
+    s)  reboot=1 
+        ;;
+    h)  echo -e "\nUsage: $script [OPTION]..."
+        echo -e "Update $distribution with the latest packages"
+        echo -e "\n\t-r\t Refresh all enabled repostiories"
+        echo -e "\t-l\t Auto agree with licenses"
+        echo -e "\t-v\t Verbosity (show maximum information)"
+        echo -e "\t-a\t Archive the log files"
+        echo -e "\t-k\t Maximum number of log files to keep,"
+        echo -e "\t\t this option must be supplied with a numeric value"
+        echo -e "\t-s\t Restart system after updates"
+        echo -e "\t-h\t Display this help message"
+        echo -e "\nExample: $script -vrk 30 "
+        echo -e "  output maximum info, reboot and keep the latest 30 log files."
+        exit 20
+        ;;
+    \?) echo -e "Invalid option: -$OPTARG\nUse $script -h for more information." >&2
         exit 30
         ;;
-    :)
-        echo -e "Option -$OPTARG requires an argument.\nUse $script -h for more information." >&2
+    :)  echo -e "Option -$OPTARG requires an argument.\nUse $script -h for more information." >&2
         exit 40
         ;;
   esac
@@ -59,26 +72,34 @@ done
 
 echo -e "Start: $date" | tee -a $log
 (( $auto_agree_with_licenses )) && { 
-    echo -e "\nApplying updates without asking to confirm licenses.\n" >&2; 
+    echo -e "\nApplying updates without asking to confirm licenses." | tee -a $log 
 }
 
-if (( $verbosity )); then
-    echo -e "\nRefreshing Repositories" | tee -a $log
-    echo -e "----------------------------------------" | tee -a $log
-    zypper refresh | cut -d"'" -f2 | tee -a $log
-    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper refresh) exiting script." >&2; exit 50; } 
-    echo -e "----------------------------------------\n" | tee -a $log
-    
-    zypper -v -n update $auto_agree_with_licenses | sed "/Unknown media type in type/d;s/^   //;/^Additional rpm output:/d" | sed ':a;N;$!ba;s/\n  / /g' | tee -a $log
-    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper update) exiting script."  >&2; exit 55; } 
-else
-    zypper refresh > /dev/nil
-    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper refresh) exiting script."  >&2; exit 60; } 
-    echo Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories
-    
+(( $refresh )) && {
+    (( $verbosity )) && {
+        echo -e "\nRefreshing Repositories" | tee -a $log
+        echo -e "----------------------------------------" | tee -a $log
+        zypper refresh | cut -d"'" -f2 | tee -a $log
+        (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper refresh) exiting script." >&2; exit 50; } 
+        echo -e "----------------------------------------\n" | tee -a $log
+    } || { 
+        zypper refresh > /dev/null; 
+        (( $? )) && { echo "An error occurred with (zypper refresh) exiting script." >&2; exit 60; } 
+        echo Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories
+    }
+}
+
+(( $verbosity )) && {
+    zypper -v -n update $auto_agree_with_licenses | sed "/Unknown media type in type/d;s/^   //;/^Additional rpm output:/d" | sed ':a;N;$!ba;s/\n  / /g' | sed ':a;N;$!ba;s/\nRetrieving: /, /g' | tee -a $log
+    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper update) exiting script." >&2; exit 55; } 
+} || {
     zypper -v -n update $auto_agree_with_licenses | grep -P "^Nothing to do|^CommitResult  \(|The following \d{1}" | sed 's/The following //' | tee -a $log
-    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper update) exiting script."  >&2; exit 65; } 
-fi
+    (( ${PIPESTATUS[0]} )) && { echo "An error occurred with (zypper update) exiting script." >&2; exit 65; } 
+}
+
+(( $maximum_log_files )) && {
+    cd $dir && ls -tp | grep -v '/$' | tail -n +$((maximum_log_files+1)) | xargs -rd '\n' rm -- 
+}
 
 s=$[$(date +%s) - $time]; h=$[$s / 3600]; s=$[$s - $[$h * 3600]]; m=$[$s / 60]; s=$[$s - $[m * 60]]
 [ "$h" != '0' ] && hours=" $h hours" || hours=""
@@ -87,4 +108,4 @@ s=$[$(date +%s) - $time]; h=$[$s / 3600]; s=$[$s - $[$h * 3600]]; m=$[$s / 60]; 
 echo -e "\nEnd: `date`\n" | tee -a $log
 echo -e "Total run time$hours$minutes $s seconds." | tee -a $log
 
-if (( $reboot )); then init 6; fi
+(( $reboot )) && init 6;

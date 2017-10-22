9c4683fa636032bf66061553337718d1c585d938
Dan Huckson <danhuckson@hotmail.com>
2017-10-22 18:59:42 -0400

    Added version variable
    Added a version variable used for outputting the current script version to the log file. Renamed some variables and moved some code around.

--- suse-update.sh
+++ suse-update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ###############################################################################
 #  Script: suse-update.sh
-# Purpose: Update openSUSE tumbleweed with the latest packages.
-# Version: 2.11
-#  Author: Dan Huckson
+# Purpose: Update openSUSE Linux with the latest packages.
+# Version: 2.12
+#  Author: Dan Huckson, https://github.com/unodan
 ###############################################################################
+version=2.12
 
 function get_time_string {
     unset time_string hours minutes seconds
@@ -17,28 +18,29 @@ function get_time_string {
     time_string=${hours}${minutes}${seconds}
 }
 
+date=`date`
 start_time=$(date +%s)
 script=$(basename -- "$0")
+script_basename=`echo $script | cut -f1 -d.`
 distribution=`cat /etc/*-release | grep ^NAME | cut -d'"' -f2`
 version_id=`cat /etc/*-release | grep ^VERSION_ID | cut -d'"' -f2`
-name=`echo $script | cut -f1 -d.`
-logs=/var/log/$name && [ ! -d "$logs" ] && mkdir $logs; 
-log=$logs/$name.log
+log_dir=/var/log/$script_basename && [ ! -d "$log_dir" ] && mkdir $log_dir
+log_file=$log_dir/$script_basename.log
 
 while getopts ":alrvk:s:h" opt; do
   case $opt in    
-    a)  number_of_log_files=`ls $logs/$name*.log 2> /dev/null | wc -l`
+    a)  number_of_log_files=`ls $log_dir/$script_basename*.log 2> /dev/null | wc -l`
     
         (( $maximum_log_files )) && (( $maximum_log_files > 0 )) && (( $number_of_log_files >= $maximum_log_files )) && {
-            [ ! -d $logs/archive ] && mkdir -p $logs/archive; 
-            cd $logs && ls *.log | xargs zip -q "archive/$name-logs-`date +%Y%m%d-%H%M%S`.zip" && rm *.log
+            [ ! -d $log_dir/archive ] && mkdir $log_dir/archive; 
+            cd $log_dir && ls *.log | xargs zip -q "archive/$script_basename-logs-`date +%Y%m%d-%H%M%S`.zip" && rm *.log
         }
         ;;
     l)  auto_agree_with_licenses="--auto-agree-with-licenses"
         ;;
     r)  refresh=1
         ;;
-    v)  verbosity=1 
+    v)  verbose=1 
         ;;
     k)  maximum_log_files=$OPTARG    
     
@@ -46,7 +48,7 @@ while getopts ":alrvk:s:h" opt; do
             echo -e "ERROR: Please enter a positive interger for the maximum number of log files to keep.\nUse $script -h for more information." >&2
             exit 10
         }
-        log="$logs/$name-`date +%Y%m%d-%H%M%S`.log"
+        log_file="$log_dir/$script_basename-`date +%Y%m%d-%H%M%S`.log"
         ;;
     s)  restart_timeout=$OPTARG
         
@@ -85,37 +87,37 @@ Example: $script -v -s 300 -k 30
   esac
 done
 
-echo -e "Start: `date`" > $log
+echo -e "$script, Version:$version, $date" | tee $log_file
 (( $auto_agree_with_licenses )) && agree_with_licenses=", accepting all licenses.\n"
-echo -e "\nApplying updates to ($distribution) Version:${version_id}${agree_with_licenses}" | tee -a $log 
+echo -e "\nApplying updates to ($distribution) Version:${version_id}${agree_with_licenses}" | tee -a $log_file 
 
 (( $refresh )) && {
-    (( $verbosity )) && {
-        echo -e "\nRefreshing Repositories" | tee -a $log
-        echo -e "----------------------------------------" | tee -a $log
-        zypper refresh | cut -d"'" -f2 | tee -a $log; err=${PIPESTATUS[0]}
-        echo -e "----------------------------------------" | tee -a $log
+    (( $verbose )) && {
+        echo -e "\nRefreshing Repositories" | tee -a $log_file
+        echo -e "----------------------------------------" | tee -a $log_file
+        zypper refresh | cut -d"'" -f2 | tee -a $log_file; err=${PIPESTATUS[0]}
+        echo -e "----------------------------------------" | tee -a $log_file
     } || zypper refresh > /dev/null; err=$?
     
     (( $err != 0 )) && { echo "An error ( $err ) occurred when refreshing repositories, exiting script." >&2; exit 60; } 
-    echo -e "Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories.\n" | tee -a $log 
+    echo -e "Refreshed `zypper repos | grep -e '| Yes ' | cut -d'|' -f3 | wc -l` repositories.\n" | tee -a $log_file 
 }
 
-if (( $verbosity )); then 
-    zypper -v -n update $auto_agree_with_licenses | sed '/Unknown media type in type/d; s/^   //; /^Additional rpm output:/d; :a;N;$!ba;s/\n  / /g' | sed ':a;N;$!ba;s/\nRetrieving: /, /g' | tee -a $log
+if (( $verbose )); then 
+    zypper -v -n update $auto_agree_with_licenses | sed '/Unknown media type in type/d; s/^   //; s/^CommitResult/\n\nCommitResult/; /^Additional rpm output:/d; :a;N;$!ba;s/\n  / /g' | sed ':a;N;$!ba;s/\nRetrieving: /, /g' | tee -a $log_file
     err=${PIPESTATUS[0]}
 else
-    zypper -v -n update $auto_agree_with_licenses | grep -P "^Nothing to do|^CommitResult  \(|The following \d{1}" | sed 's/The following //' | tee -a $log
+    zypper -v -n update $auto_agree_with_licenses | grep -P "^Nothing to do|^CommitResult  \(|The following \d{1}" | sed 's/The following //' | tee -a $log_file
     err=${PIPESTATUS[0]}
 fi
 
 (( $err != 0 )) && { echo "An error ( $err ) occurred with ( $script ) exiting script." >&2; exit 70; } 
-(( $maximum_log_files )) && cd $logs && ls -tp | grep -v '/$' | tail -n +$((maximum_log_files+1)) | xargs -rd '\n' rm -- 
+(( $maximum_log_files )) && cd $log_dir && ls -tp | grep -v '/$' | tail -n +$((maximum_log_files+1)) | xargs -rd '\n' rm -- 
 
-sed -i 's/   dracut:/\ndracut:/g; s/^CommitResult/\n\nCommitResult/; /^Checking for running processes/d; /^There are some running programs/d' $log
-
+echo -e "\nStarted:  $date\n\nFinished: `date`\n" | tee -a $log_file
 get_time_string $[$(date +%s) - $start_time]
-echo -e "\nEnd: `date`\n\nTotal run time$time_string." | tee -a $log
+echo -e "Total run time$time_string." | tee -a $log_file
+sed -i 's/   dracut:/\ndracut:/g; s/^CommitResult/\nCommitResult/; /^Checking for running processes/d; /^There are some running programs/d' $log_file
 
 (( $restart_timeout )) && { 
     get_time_string $restart_timeout
@@ -125,21 +127,20 @@ echo -e "\nEnd: `date`\n\nTotal run time$time_string." | tee -a $log
         xmessage "     * * * Warnning restarting the system in$time_string * * *     " -timeout $restart_timeout -button " Restart , Cancel " &> /dev/null
         err=$?
         (( $err == 0 )) || (( $err == 101 )) && {
-            echo -e "1: System was restarted. ($(date))\n" >> $log
+            echo -e "Restarted: `date`\n" >> $log_file
             init 6
         }
-        echo | tee -a $log
-        zypper ps -s | sed '/No processes using deleted files found/d' | tee -a $log
-        echo -e "System restart was canceled. ($(date))\n" | tee -a $log
+        zypper ps -s | sed '/No processes using deleted files found/d' | tee -a $log_file
+        echo -e "\nRestart Canceled: `date`\n" >> $log_file
     else 
         sleep $restart_timeout
-        echo -e "2: System was restarted. ($(date))\n" >> $log
+        echo -e "Restarted: `date`\n" >> $log_file
         init 6
     fi
 } || {
     if [[ $restart_timeout == 0 ]]; then
         if xhost > /dev/null 2>&1; then gui=3; else gui=4; fi
-        echo -e "$gui: System was restarted. ($(date))\n" >> $log
+        echo -e "Restarted: `date`\n" >> $log_file
         init 6
     fi
 }
